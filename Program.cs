3704fb10ef4e644f6f343e1ab10f8e8f8e95cd2d
Darek <dariuszgrados@gmail.com>
2022-08-29 23:29:47 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
+        public static bool Solution(string input)
+        {
+            //deklaracja stosu do odkładania/zdejmowania znaków
+            Stack<char> brackets = new Stack<char>();
+            Dictionary<char, char> bracketPairs = new Dictionary<char, char>()
+            {
+                {'(', ')' },
+                {'[', ']' },
+                {'{', '}' },
+                {'<', '>' },
+            };
+
+            try
+            {
+                foreach (var mark in input)
+                {
+                    if (bracketPairs.Keys.Contains(mark))
+                        brackets.Push(mark);
+                    else if (bracketPairs.Values.Contains(mark))
+                    {
+                        if (mark == bracketPairs[brackets.First()])
+                            brackets.Pop();
+                        else
+                            return false;
+                    }
+                    else
+                        continue;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return brackets.Count() == 0 ? true : false;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter string: ");
+            string expresion = Console.ReadLine();
+            Console.WriteLine(Solution(expresion));
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # brackets_in_string
+short app which is checking if your for example .xml or .json file is correct 
+brackets are written correctly like {}{}(())) 
